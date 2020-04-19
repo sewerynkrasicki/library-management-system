f02deb09c3c884d6d95259d507017e9c9d32bbc3
Seweryn Krasicki <seweryn.krasicki@protonmail.com>
2020-04-19 16:21:19 +0200

    Author POJO and hbm

---
+++ library-management-system/src/main/java/POJO/Autor.java
@@ -0,0 +1,77 @@
+package POJO;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 35747
+ */
+@Entity
+public class Autor {
+    @Id
+    private int id;
+    private String imie;
+    private String nazwisko;
+    private String miejsceUrodzenia;
+    private String wiekDzialania;
+    
+    public Autor(){
+        
+    }
+    
+    public Autor(String imie, String nazwisko, String miejsceUrodzenia, String wiekDzialania){
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.miejsceUrodzenia = miejsceUrodzenia;
+        this.wiekDzialania = wiekDzialania;
+    }
+    
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public String getMiejsceUrodzenia() {
+        return miejsceUrodzenia;
+    }
+
+    public String getWiekDzialania() {
+        return wiekDzialania;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void setMiejsceUrodzenia(String miejsceUrodzenia) {
+        this.miejsceUrodzenia = miejsceUrodzenia;
+    }
+
+    public void setWiekDzialania(String wiekDzialania) {
+        this.wiekDzialania = wiekDzialania;
+    }
+}
+
